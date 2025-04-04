4026882f1e20ab4e572966069c26e9d5bc50142a
Juan Sebastian Bustos <juan_seb07@hotmail.com>
2025-04-03 23:07:33 -0500

    add initial logic to GenerateInfoFiles and add the input folders with the example files

--- src/reports/GenerateInfoFiles.java
+++ src/reports/GenerateInfoFiles.java
@@ -1,11 +1,82 @@
 package reports;
 
+import java.io.*;
+
 public class GenerateInfoFiles {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		// Ruta donde se encuentra todos los archivos con la información de los vendedores.
+		String pathFileSellers = "files/input/sellers";
+		
+		processFilesInFolder(pathFileSellers);
+		
+		// Ruta donde se encuentra el archivo con la información de las ventas.
+		String pathFileSales = "files/input/sales";
+		
+		processFilesInFolder(pathFileSales);
+				
+		// Ruta donde se encuentra el archivo con la información de los productos disponibles.
+		String pathFileProducts = "files/input/products";
+		
+		processFilesInFolder(pathFileProducts);
 		
 	}
+	
+	public static void readFile(File nameFile) {
+		try {
+            BufferedReader br = new BufferedReader(new FileReader(nameFile));
+            String line;
+
+            System.out.println("Contenido procesado del archivo:");
+            
+            // Leer línea por línea
+            while ((line = br.readLine()) != null) { 
+            	
+            	// Separar por ";"
+                String[] partes = line.split(";"); 
+
+                // Mostrar cada parte en la consola
+                for (String parte : partes) {
+                    System.out.print(parte + " | "); 
+                }
+                
+                // Salto de línea para mejor visualización
+                System.out.println(); 
+            }
+            
+            br.close();
+            
+        } catch (IOException e) {
+            System.out.println("Error al leer el archivo: " + e.getMessage());
+        }
+	}
+	
+    public static void processFilesInFolder(String folderPath) {
+    	File folder = new File(folderPath);
+        
+        // Verificar si la carpeta existe y es un directorio
+        if (!folder.exists() || !folder.isDirectory()) {
+            System.out.println("El folder no existe o no es un directorio valido. Por favor, verifica he intenta nuevamente.");
+            return;
+        }
+
+        // Obtener la lista de archivos dentro de la carpeta
+        File[] files = folder.listFiles();
+
+        if (files == null || files.length == 0) {
+            System.out.println("La carpeta esta vacia. Por favor, agrega los archivos he intenta nuevamente.");
+            return;
+        }
+
+        // Iterar sobre cada archivo en la carpeta
+        for (File file : files) {
+            if (file.isFile()) {
+                System.out.println("\nNombre del archivo plano: " + file.getName());
+                readFile(file);
+            }
+        }
+    }
 
 }
 
