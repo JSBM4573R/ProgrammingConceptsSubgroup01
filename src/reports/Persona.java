e45b4da5b9abc2b055b2ab9c3e317ada04290f05
Juan Sebastian Bustos <juan_seb07@hotmail.com>
2025-04-05 21:53:27 -0500

    add basic logic in method main

---
+++ src/reports/Persona.java
@@ -0,0 +1,29 @@
+package reports;
+
+public class Persona {
+
+	private String name;
+	private int edad;
+	
+	public void saludar() {
+		System.out.println("Hola");
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	
+	
+}
