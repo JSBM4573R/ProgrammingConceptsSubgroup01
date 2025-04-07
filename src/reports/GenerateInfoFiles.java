53aaac85b3d3bc8d2073e307b3b14da1aadd2812
Juan Sebastian Bustos <juan_seb07@hotmail.com>
2025-04-06 23:39:42 -0500

    add logic for method main and include test

--- src/reports/GenerateInfoFiles.java
+++ src/reports/GenerateInfoFiles.java
@@ -8,39 +8,37 @@ public class GenerateInfoFiles {
 	public static void main(String[] args) {
 
 		String pathGenerateFileProducts = "files/input/products/InfoProductosDisponibles.csv";
-		// Crea un archivo plano con los productos disponibles
+		// Creamos un archivo plano con los productos disponibles
 		writeFiles(pathGenerateFileProducts, infoProducts());
 
 		String pathGenerateFileSellers = "files/input/sellers/InfoVendedores.csv";
-		// Crear un archivo plano con información de los vendedores
+		// Creamos un archivo plano con información de los vendedores
 		writeFiles(pathGenerateFileSellers, infoSellers());
 
 		String pathGenerateFileSalesCamila = "files/input/sales/VentasCamila.csv";
-		// Crea un archivo plano con las ventas de Camila
+		// Creamos un archivo plano con las ventas de Camila
 		writeFiles(pathGenerateFileSalesCamila, infoSaleCamila());
 
 		String pathGenerateFileSalesSantiago = "files/input/sales/VentasSantiago.csv";
-		// Crea un archivo plano con las ventas de Santiago
+		// Creamos un archivo plano con las ventas de Santiago
 		writeFiles(pathGenerateFileSalesSantiago, infoSaleSantiago());
 
 		String pathGenerateFileSalesMateo = "files/input/sales/VentasMateo.csv";
-		// Crea un archivo plano con las ventas de Mateo
+		// Creamos un archivo plano con las ventas de Mateo
 		writeFiles(pathGenerateFileSalesMateo, infoSaleMateo());
 
 		System.out.println(
-				"¡Finalización Exitosa de la clase GenerateInfoFiles! Se generaron los archivos planos en la ruta: files/input/*");
+				"¡Finalización Exitosa de la clase GenerateInfoFiles! Se generaron todos los archivos planos en la ruta: files/input/*");
+
 	}
 
 	public static void writeFiles(String filePath, List<String> dataList) {
-		// Usamos la clase BufferedWriter para poder usar sus metodos y escribir un
-		// archivo plano
-
+		// Usamos la clase BufferedWriter escribir un archivo
 		try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(filePath))) {
 
 			// Usamos un bucle for para recorrer la lista de datos que nosotros creamos
 			for (int i = 0; i < dataList.size(); i++) {
-				// Usamos el metodo write del BufferedWriter para escribir las lineas del
-				// archivo
+				// Usamos el metodo write del BufferedWriter para escribir las lineas
 				bufferedWriter.write(dataList.get(i));
 				// Valida que el ultimo registro en el archivo plano no genere un salto de linea
 				if (i != dataList.size() - 1) {
@@ -51,73 +49,82 @@ public class GenerateInfoFiles {
 
 		} catch (IOException e) {
 			System.out.println("Error al escribir el archivo: " + e.getMessage());
+			return;
 		}
 	}
 
 	public static List<String> infoProducts() {
 		List<String> listProducts = new ArrayList<String>();
-		
-		String product1 = "001;Esfero;2500";
+
+		String product1 = "1;Esfero;2500";
 		listProducts.add(product1);
-		String product2 = "003;Lápiz;1950";
+		String product2 = "3;Lápiz;1950";
 		listProducts.add(product2);
-		String product3 = "004;Borrador;850";
+		String product3 = "4;Borrador;850";
 		listProducts.add(product3);
-		String product4 = "002;Tajalápiz;1200";
+		String product4 = "2;Tajalápiz;1200";
 		listProducts.add(product4);
-		String product5 = "005;Escuadra;5800";
+		String product5 = "5;Escuadra;5800";
 		listProducts.add(product5);
-		
+
 		return listProducts;
 	}
 
 	public static List<String> infoSaleCamila() {
 		List<String> listSales = new ArrayList<String>();
+
 		String encabezado = "CC;1023456789";
 		listSales.add(encabezado);
-		String sale1 = "002;5";
+		String sale1 = "2;5";
 		listSales.add(sale1);
-		String sale2 = "001;3";
+		String sale2 = "1;3";
 		listSales.add(sale2);
-		String sale3 = "003;7";
+		String sale3 = "3;7";
 		listSales.add(sale3);
+
 		return listSales;
 	}
 
 	public static List<String> infoSaleSantiago() {
 		List<String> listSales = new ArrayList<String>();
+
 		String encabezado = "CC;1019156723";
 		listSales.add(encabezado);
-		String sale1 = "003;8";
+		String sale1 = "3;8";
 		listSales.add(sale1);
-		String sale2 = "002;4";
+		String sale2 = "2;4";
 		listSales.add(sale2);
-		String sale3 = "001;6";
+		String sale3 = "1;6";
 		listSales.add(sale3);
+
 		return listSales;
 	}
 
 	public static List<String> infoSaleMateo() {
 		List<String> listSales = new ArrayList<String>();
+
 		String encabezado = "CC;1018456735";
 		listSales.add(encabezado);
-		String sale1 = "001;1";
+		String sale1 = "1;1";
 		listSales.add(sale1);
-		String sale2 = "003;7";
+		String sale2 = "3;7";
 		listSales.add(sale2);
-		String sale3 = "002;4";
+		String sale3 = "2;4";
 		listSales.add(sale3);
+
 		return listSales;
 	}
 
 	public static List<String> infoSellers() {
 		List<String> listSellers = new ArrayList<String>();
+
 		String seller1 = "CC;1023456789;Camila;Buitrago";
 		listSellers.add(seller1);
 		String seller2 = "CC;1019156723;Santiago;Torres";
 		listSellers.add(seller2);
 		String seller3 = "CC;1018456735;Mateo;Peralta";
 		listSellers.add(seller3);
+
 		return listSellers;
 	}
 
--- src/reports/Main.java
+++ src/reports/Main.java
@@ -1,61 +1,179 @@
 package reports;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
+	public static List<Product> products = new ArrayList<Product>();
+	public static List<Seller> sellers = new ArrayList<Seller>();
+	public static List<Sale> sales = new ArrayList<Sale>();
+
 	public static void main(String[] args) {
 
-		// Ruta donde se encuentra todos los archivos con la información de los vendedores.
+		// Ruta del archivo con la información de los vendedores
 		String pathFileSellers = "files/input/sellers";
-
 		processFilesInFolder(pathFileSellers);
 
-		// Ruta donde se encuentra el archivo con la información de las ventas.
-		String pathFileSales = "files/input/sales";
-
-		processFilesInFolder(pathFileSales);
-
-		// Ruta donde se encuentra el archivo con la información de los productos disponibles.
+		// Ruta del archivo con la información de los productos
 		String pathFileProducts = "files/input/products";
-
 		processFilesInFolder(pathFileProducts);
 
-		System.out.println("¡Finalización Exitosa! Se procesaron todos los archivos planos pseudoaleatorios en la carpeta files.");
+		// Ruta donde se encuentran los archivos con la información de las ventas
+		String pathFileSales = "files/input/sales";
+		processFilesInFolder(pathFileSales);
+
+		// Generamos los test
+		testFiles();
+
+		System.out.println(
+				"¡Finalización Exitosa de los test! Se generaron todos los archivos de test en la ruta: files/test/*");
+
+		// Generamos el reporte total de ventas por cada vendedor
+		generateSalesReport();
+
+		// Generamos el reporte total de la cantidad de productos vendidos
+		generateSalesProductsReport();
+
+		System.out.println(
+				"¡Finalización Exitosa de la clase Main! Se generaron todos los reportes en la ruta: files/output/*");
+		
+		System.out.println(
+				"¡No olvides de darle un Refresh a la carpeta raiz para visualizarlos!");
+
 	}
 
 	public static void readFile(File nameFile) {
+
 		try {
 			BufferedReader bufferedReader = new BufferedReader(new FileReader(nameFile));
 			String line;
 
-			System.out.println("Contenido que se logro leer del archivo:");
+			int sequence = 1;
+			int idSeller = 0;
 
 			// Leer línea por línea
 			while ((line = bufferedReader.readLine()) != null) {
 
-				// Separar por ";"
+				// Separamos por ";"
 				String[] partes = line.split(";");
 
-				// Mostrar cada parte en la consola
-				for (String parte : partes) {
-					System.out.print(parte + " | ");
+				if (nameFile.toString().contains("sellers")) { // Filtramos por el archivo de vendedores
+					sellers.add(mapperSellers(partes, sequence));
+					++sequence;
+				} else if (nameFile.toString().contains("products")) { // Filtramos por el archivo de productos
+					products.add(mapperAvailableProducts(partes));
+				} else if (nameFile.toString().contains("sales")) { // Filtramos por el archivo de ventas
+					if (partes[0].contains("CC")) { // Identificamos el vendedor que esta en la primera linea
+						Seller seller = findSellerByDocumentNumber(sellers, partes[1]);
+						idSeller = seller.getId();
+					} else {
+						sales.add(mapperSales(partes, sequence, idSeller));
+						++sequence;
+					}
 				}
-
-				// Salto de línea para mejor visualización
-				System.out.println();
 			}
 
+			// Reiniciamos la secuencia
+			sequence = 1;
+
+			// Limpiamos el buffered en memoria
 			bufferedReader.close();
 
 		} catch (IOException e) {
 			System.out.println("Error al leer el archivo: " + e.getMessage());
 		}
+	}
+
+	public static void generateSalesReport() {
+		Seller seller = null;
+		String pathGenerateFileSalesReport = "files/output/reporteTotalVentas.csv";
+		Map<String, Double> salesPerSeller = new HashMap<>();
+
+		// Recorremos la lista de ventas generadas
+		for (Sale item : sales) {
+			seller = findSellerById(sellers, item.getIdSeller());
+			if (seller != null) {
+				// Multiplicamos el precio unitario por la cantidad de producto vendido
+				double valueSale = item.getProductAmount()
+						* findProductById(products, item.getProduct().getId()).getUnitPrice();
+
+				String key = seller.getName() + ";" + seller.getLastName();
+
+				// Acumulamos la venta por vendedor
+				salesPerSeller.put(key, salesPerSeller.getOrDefault(key, 0.0) + valueSale);
+			}
+		}
+
+		// Creamos una lista con las entradas del Map
+		List<Map.Entry<String, Double>> orderedList = new ArrayList<>(salesPerSeller.entrySet());
+
+		// Ordenamos con Comparator clasico (de mayor a menor)
+		Collections.sort(orderedList, new Comparator<Map.Entry<String, Double>>() {
+			public int compare(Map.Entry<String, Double> valueOne, Map.Entry<String, Double> valueTwo) {
+				return Double.compare(valueTwo.getValue(), valueOne.getValue());
+			}
+		});
+
+		List<String> lineas = new ArrayList<>();
+		// Incluimos una cabezera para identificar cada columna en el archivo
+		String header = "NOMBRE;APELLIDO;TOTAL VENTAS";
+		lineas.add(header);
+
+		// Usamos un for each para poder recorrer la lista ordenada
+		for (Map.Entry<String, Double> entry : orderedList) {
+			String finalLine = entry.getKey() + ";" + entry.getValue();
+			lineas.add(finalLine);
+		}
+
+		// Re-utilizamos el metodo writeFiles de la clase GenerateInfoFiles
+		GenerateInfoFiles.writeFiles(pathGenerateFileSalesReport, lineas);
+	}
+
+	public static void generateSalesProductsReport() {
+		Map<Integer, Integer> saleAmountProducts = new HashMap<Integer, Integer>();
+		Map<String, Integer> salesPerAmountProduct = new HashMap<>();
+		String pathGenerateFileSalesProductReport = "files/output/reporteCantidadTotalProductosVendidos.csv";
+
+		// Recorremos la lista de ventas generadas
+		for (Sale sale : sales) {
+			// Buscamos el producto por el id
+			Product product = findProductById(products, sale.getProduct().getId());
+			if (!saleAmountProducts.containsKey(product.getId())) {
+				// Si no encontramos el producto en el Map lo agregamos con la cantidad inicial
+				saleAmountProducts.put(product.getId(), sale.getProductAmount());
+			} else {
+				// Si encontramos el producto en el Map unicamente sumamos la cantidad
+				saleAmountProducts.merge(product.getId(), sale.getProductAmount(), Integer::sum);
+
+			}
+			String key = product.getName() + ";" + product.getUnitPrice();
+			salesPerAmountProduct.put(key, saleAmountProducts.get(product.getId()));
+		}
+
+		// Creamos una lista con las entradas del Map
+		List<Map.Entry<String, Integer>> orderedList = new ArrayList<>(salesPerAmountProduct.entrySet());
+
+		// Ordenamos con Comparator clasico (de mayor a menor)
+		Collections.sort(orderedList, new Comparator<Map.Entry<String, Integer>>() {
+			public int compare(Map.Entry<String, Integer> valueOne, Map.Entry<String, Integer> valueTwo) {
+				return Integer.compare(valueTwo.getValue(), valueOne.getValue());
+			}
+		});
+
+		List<String> lines = new ArrayList<>();
+		// Incluimos una cabezera para identificar cada columna en el archivo
+		String header = "NOMBRE PRODUCTO;PRECIO UNITARIO;CANTIDAD VENDIDA";
+		lines.add(header);
+
+		// Usamos un for each para poder recorrer la lista ordenada
+		for (Map.Entry<String, Integer> entry : orderedList) {
+			String finalLine = entry.getKey() + ";" + entry.getValue();
+			lines.add(finalLine);
+		}
+
+		// Re-utilizamos el metodo writeFiles de la clase GenerateInfoFiles
+		GenerateInfoFiles.writeFiles(pathGenerateFileSalesProductReport, lines);
 	}
 
 	public static void processFilesInFolder(String folderPath) {
@@ -76,25 +194,147 @@ public class Main {
 			return;
 		}
 
-		// Iterar sobre cada archivo en la carpeta
+		// Iterar sobre cada archivo en la carpeta y leerlo
 		for (File file : files) {
 			if (file.isFile()) {
-				System.out.println("\nNombre del archivo plano: " + file.getName());
 				readFile(file);
 			}
 		}
 
 	}
 
-	public List<Product> mapperProductosDisponibles(String[] partes) {
-		List<Product> listProducts = new ArrayList<Product>();
-		Product product = new Product();
-		product.setId(Integer.valueOf(partes[0]));
-		product.setName(partes[1]);
-
-		return listProducts;
+	public static Seller findSellerByDocumentNumber(List<Seller> listSeller, String documentNumber) {
+		for (Seller item : listSeller) {
+			if (item.getDocumentNumber().equals(documentNumber)) {
+				return item;
+			}
+		}
+		return null;
 	}
 
+	public static Product findProductById(List<Product> listProduct, int id) {
+		for (Product item : listProduct) {
+			if (item.getId().equals(id)) {
+				return item;
+			}
+		}
+		return null;
+	}
+
+	public static Seller findSellerById(List<Seller> listSeller, int id) {
+		for (Seller item : listSeller) {
+			if (item.getId().equals(id)) {
+				return item;
+			}
+		}
+		return null;
+	}
+
+	public static Product mapperAvailableProducts(String[] parts) {
+		Product product = new Product();
+
+		product.setId(Integer.valueOf(parts[0]));
+		product.setName(parts[1]);
+		product.setUnitPrice(Double.valueOf(parts[2]));
+
+		return product;
+	}
+
+	public static Sale mapperSales(String[] parts, int idSale, int idSeller) {
+		Sale sale = new Sale();
+
+		sale.setId(idSale);
+		sale.setIdSeller(idSeller);
+		Product product = new Product();
+		product.setId(Integer.valueOf(parts[0]));
+		sale.setProduct(product);
+		sale.setProductAmount(Integer.valueOf(parts[1]));
+
+		return sale;
+	}
+
+	public static Seller mapperSellers(String[] parts, int id) {
+		Seller seller = new Seller();
+
+		seller.setId(id);
+		seller.setDocumentType(parts[0]);
+		seller.setDocumentNumber(parts[1]);
+		seller.setName(parts[2]);
+		seller.setLastName(parts[3]);
+
+		return seller;
+	}
+
+	/**
+	 * Para propósitos de prueba, se crean métodos de generación de archivos de
+	 * prueba para el programa en cuestión.
+	 */
+	public static void testFiles() {
+		Random random = new Random();
+		createSalesMenFile(random.nextInt(3)+1, "Andres", Long.valueOf(1));
+		createProductsFile(products.size());
+		createSalesManInfoFile(random.nextInt(3)+1);
+	}
+
+	/**
+	 * 5. Archivos de prueba a. Dada una cantidad, un nombre y un id, crea un
+	 * archivo pseudoaleatorio de ventas de un vendedor con el nombre y el id dados.
+	 * 
+	 * @param randomSalesCount
+	 * @param name
+	 * @param id
+	 */
+	public static void createSalesMenFile(int randomSalesCount, String name, long id) {
+		String testPathCreateSalesMenFile = "files/test/createSalesMenFile.csv";
+		List<String> listSales = new ArrayList<String>();
+
+		String header = String.valueOf(id) + ";" + name;
+		listSales.add(header);
+		for (int i = 0; i <= randomSalesCount; i++) {
+			listSales.add(products.get(i).getId() + ";" + i);
+		}
+
+		GenerateInfoFiles.writeFiles(testPathCreateSalesMenFile, listSales);
+	}
+
+	/**
+	 * 5. Archivos de prueba b. crea un archivo con información pseudoaleatoria de
+	 * productos, con los datos de productsCount productos.
+	 * 
+	 * @param productsCount
+	 */
+	public static void createProductsFile(int productsCount) {
+		String testPathCreateProductsFile = "files/test/createProductsFile.csv";
+		List<String> listSales = new ArrayList<String>();
+
+		for (int i = 0; i < productsCount; i++) {
+			listSales.add(
+					products.get(i).getId() + ";" + products.get(i).getName() + ";" + products.get(i).getUnitPrice());
+		}
+
+		GenerateInfoFiles.writeFiles(testPathCreateProductsFile, listSales);
+	}
+
+	/**
+	 * 5. Archivos de prueba c. crea un archivo con información de salesmanCount
+	 * vendedores; el número de estos según lo indique el argumento entero. La
+	 * información debe ser generada de manera pseudoaleatoria y ser coherente, es
+	 * decir, los nombres y apellidos pueden ser extraídos de listas de nombres
+	 * reales de personas.
+	 * 
+	 * @param salesmanCount
+	 */
+	public static void createSalesManInfoFile(int salesmanCount) {
+		String testPathCreateSalesManInfoFile = "files/test/createSalesManInfoFile.csv";
+		List<String> listSales = new ArrayList<String>();
+		Random random = new Random();
+		for (int i = 0; i < salesmanCount; i++) {
+			listSales.add("CC" + 101914510 + random.nextInt(9)+1 + ";" + sellers.get(random.nextInt(2)+1).getName() + ";"
+					+ sellers.get(random.nextInt(2)+1).getLastName());
+		}
+
+		GenerateInfoFiles.writeFiles(testPathCreateSalesManInfoFile, listSales);
+	}
 }
 
 class Seller {
@@ -104,7 +344,6 @@ class Seller {
 	private String lastName;
 	private String documentType;
 	private String documentNumber;
-	private Double salesAmount;
 
 	public Integer getId() {
 		return id;
@@ -146,18 +385,10 @@ class Seller {
 		this.documentNumber = documentNumber;
 	}
 
-	public Double getSalesAmount() {
-		return salesAmount;
-	}
-
-	public void setSalesAmount(Double salesAmount) {
-		this.salesAmount = salesAmount;
-	}
-
 	@Override
 	public String toString() {
 		return "Seller [id=" + id + ", name=" + name + ", lastName=" + lastName + ", documentType=" + documentType
-				+ ", documentNumber=" + documentNumber + ", salesAmount=" + salesAmount + "]";
+				+ ", documentNumber=" + documentNumber + "]";
 	}
 
 }
@@ -203,7 +434,8 @@ class Sale {
 
 	private Integer id;
 	private Integer idSeller;
-	private List<Product> listProducts;
+	private Product product;
+	private Integer productAmount;
 
 	public Integer getId() {
 		return id;
@@ -221,17 +453,26 @@ class Sale {
 		this.idSeller = idSeller;
 	}
 
-	public List<Product> getListProducts() {
-		return listProducts;
+	public Product getProduct() {
+		return product;
 	}
 
-	public void setListProducts(List<Product> listProducts) {
-		this.listProducts = listProducts;
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+	public Integer getProductAmount() {
+		return productAmount;
+	}
+
+	public void setProductAmount(Integer productAmount) {
+		this.productAmount = productAmount;
 	}
 
 	@Override
 	public String toString() {
-		return "Sale [id=" + id + ", idSeller=" + idSeller + ", listProducts=" + listProducts + "]";
+		return "Sale [id=" + id + ", idSeller=" + idSeller + ", product=" + product + ", productAmount=" + productAmount
+				+ "]";
 	}
 
 }
